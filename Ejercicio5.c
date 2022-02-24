d7f7d19cc5e7882928b05861e787778cb9d01411
Jacobo-EG <0619975360@uma.es>
2022-02-24 20:09:46 +0100

    He realizado el ejercicio5

--- Ejercicio5.c
+++ Ejercicio5.c
@@ -7,10 +7,40 @@ i para iniciar
 2 para opción de llamada1
 f para fin
 
-Tras leer, imprimimos un mensaje, elige el mensaje que quieras. Cuando se lee un f de fin, se termina el programa. Si se lee algo que no corresponde con una opción de menú, se avisa al usuario que no se conoce esa opción, y se le muestra lo que ha introducido.
+Tras leer, imprimimos un mensaje, elige el mensaje que quieras. Cuando se lee un f de fin, se termina el programa.
+ Si se lee algo que no corresponde con una opción de menú, se avisa al usuario que no se conoce esa opción, 
+ y se le muestra lo que ha introducido.
 
 //Usa para hacerlo:
 int getchar(void)
 int putchar (int character);
 switch
 */
+#include<stdio.h>
+#include<string.h>
+int main(int argc, char const *argv[])
+{
+    char caracter, inicio='i', fin='f', llamada1='1',llamada2='2';
+
+    printf("Teclee i para iniciar: ");
+    caracter=getchar();
+    if(caracter == inicio){
+        printf("Seleccione 1 para llamada 1, 2 para llamada2, 2 para la llamada 2, o f para finalizar");
+        caracter = getchar();
+        while(caracter!=fin){
+            if(caracter == llamada1){
+                printf("Llamada1");
+            }else if(caracter == llamada2){
+                printf("Llamada2");
+            }else{
+                printf("No se conoce el caracter que ha introducido: ");
+                putchar(caracter);
+            }
+            caracter=getchar();
+        }
+
+    }else{
+        printf("No se ha iniciado el menú");
+    }
+    return 0;
+}
