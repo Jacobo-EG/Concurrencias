55d239b6c5a40cabdeb5fe5884a789b6a3b0191c
Jacobo-EG <0619975360@uma.es>
2022-02-24 21:01:10 +0100

    Primer intento ejercicio92

--- Ejercicio9.c
+++ Ejercicio9.c
@@ -38,3 +38,69 @@ struct str_Revisa_Mensual
     enum Meses mes;
     float precio;
 };
+
+typedef struct str_Libro Libro;
+typedef struct str_Revisa_Mensual Revista;
+
+struct str_item
+{
+    Libro libro;
+    Revista revista;
+    char tipo;
+};
+
+typedef struct str_item Item;
+
+int main(int argc, char const *argv[])
+{
+    Item tienda[100];
+    int i=0;
+    char car,elem;
+
+    printf("Introduce el caracter i para insertar un nuevo elemento, o v para verlos todos. Finaliza con f");
+    car = getchar();
+    while(car!='f'){
+        if(car=='i'){
+            printf("Introduce r si se trata de una revista o l si se trata de un libro: ");
+            elem=getchar();
+            if(elem=='l'){
+                tienda[i].tipo=elem;
+                printf("Introduce el titulo del libro: ");
+                scanf("%200s",&tienda[i].libro.titulo);
+                printf("Introduce el autor: ");
+                scanf("%200s",&tienda[i].libro.autor);
+                printf("Introduce el numero de paginas: ");
+                scanf("%i",&tienda[i].libro.paginas);
+                printf("Introduce el precio: ");
+                scanf("%f",&tienda[i].libro.precio);
+                i++;
+            }else if(elem=='r'){
+                tienda[i].tipo=elem;
+                printf("Introduce el titulo de la revista: ");
+                scanf("%200s",&tienda[i].revista.titulo);
+                printf("Introduce el mes: ");
+                scanf("%3s",&tienda[i].revista.mes);
+                printf("Introduce el precio: ");
+                scanf("%f",&tienda[i].revista.precio);
+                i++;
+            }else{
+                printf("No hay elemento de tipo: ");
+                putchar(elem);
+            }
+
+        }else if(car=='v'){
+            for(int j=0;j<i;j++){
+                if(tienda[i].tipo=='l'){
+                    printf("El elemento %i es un libro con titulo %s, del autor %s, con %i paginas y precio %f",j,tienda[j].libro.titulo,tienda[j].libro.autor,tienda[j].libro.paginas,tienda[j].libro.precio);
+                }else{
+                    printf("El elemento %i es una revista con titulo %s, del mes %s y con precio %f",tienda[j].revista.titulo,tienda[j].revista.mes,tienda[j].revista.precio);
+                }
+            }
+        }else{
+            printf("No hay accion definida para el caracter ");
+            putchar(car);
+        }
+        car=getchar();
+    }
+    return 0;
+}
