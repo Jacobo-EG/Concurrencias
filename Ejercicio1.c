4d0c5dc339fdf6ee72a89745c42c863a3e8c7324
Jacobo Elicha Garrucho <77748843+Jacobo-EG@users.noreply.github.com>
2022-02-24 10:42:15 +0100

    Add files via upload

---
+++ Ejercicio1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main(int argc, char const *argv[])
+ {
+    char cadena[11]="HOla mundo";
+    int a = 65;
+    float b = 65.0;
+    printf("Esto es una cadena %s un entero %i y un float %f\n",cadena,a,b);
+    return 0;
+ }
