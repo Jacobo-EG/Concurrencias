31443b716d89ab6dff0aea26ecb24c6c23614bdd
Jacobo-EG <0619975360@uma.es>
2022-05-06 14:32:47 +0200

    Problema lectores-escritores y problema trabajadores

--- barberodormilon/barberodormilon/Barberia.java
+++ barberodormilon/barberodormilon/Barberia.java
@@ -40,6 +40,5 @@ public class Barberia {
 			scNum.release();
 		}
 		
-
 	}
 }
---
+++ barrera/barrera/Barrera.java
@@ -0,0 +1,31 @@
+package barrera;
+
+import java.util.concurrent.*;
+
+public class Barrera implements IBarrera {
+
+	private int tam; // Cuantos trabajadores hay en total
+	private int num = 0; // Cuantos han terminado en esta iteración
+	private Semaphore scNum = new Semaphore(1, true); // Control SC variable NUM
+	private Semaphore continua = new Semaphore(0, true); // Permiso para continuar, al principio nadie tiene permiso
+
+	public Barrera(int tam) {
+		this.tam = tam;
+	}
+
+	public void terminoIteracion(int id) throws InterruptedException {
+		scNum.acquire();
+		System.out.println("Ha terminado " + id + " y en total son " + num);
+		if(num >= tam-1){
+			num = 0;
+			scNum.release();
+			continua.release(tam-1);
+		}else{
+			num++;
+			scNum.release();
+			continua.acquire();
+		}
+		
+	}
+
+}
