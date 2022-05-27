44726249d4bf406d8e0ef75c8e321e9a21297c07
Jacobo-EG <0619975360@uma.es>
2022-05-27 19:41:15 +0200

    Empiezo a realizar examen junio 2018, la parte sobre concurrencias

---
+++ ExamenesJava/Junio18/agua/GestorAgua.java
@@ -0,0 +1,16 @@
+package agua;
+
+
+import java.util.concurrent.*;
+
+public class GestorAgua {
+
+	
+	
+	public void hListo(int id) throws InterruptedException{ 
+	}
+	
+	public void oListo(int id) throws InterruptedException{ 
+
+	}
+}
